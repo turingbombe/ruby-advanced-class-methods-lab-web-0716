62352d43a96403de9f985c7b556e329895173fb4
Charlie Cook <turingbombe@gmail.com>
2016-07-29 12:40:06 -0400

    Added all completed tasks

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,68 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
+  
   @@all = []
 
+  def self.new_by_name(name)
+    song = self.new
+    song.name= name
+    song
+  end
+  
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
   def self.all
     @@all
   end
+
+  def self.find_by_name(song)
+    self.all.detect{|all| all.name == song}
+  end
+
+  def self.find_or_create_by_name(song)
+    if self.find_by_name(song) !=nil
+      self.find_by_name(song)
+    else
+      self.create_by_name(song)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|x| x.name}
+  end
+  def self.new_from_filename(file)
+    song_components = file.split(" - ")
+    song = self.new
+    song.name = song_components[1].chomp(".mp3")
+    song.artist_name = song_components[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song_components = file.split(" - ")
+    song = self.new
+    song.name = song_components[1].chomp(".mp3")
+    song.artist_name = song_components[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
 
   def save
     self.class.all << self
